7295d5ad6b23c2fd55e87fa9b7e6ffe13489dc99
dev003.binarybrix@gmail.com <146065152+NabeelArshad1@users.noreply.github.com>
2024-10-23 19:54:31 +0500

    changing in treatment1 & treatment2

--- src/components/logosection/LogoSection.jsx
+++ src/components/logosection/LogoSection.jsx
@@ -3,32 +3,43 @@ import logo2 from '../../images/logo2.svg';
 import logo3 from '../../images/logo3.svg';
 import logo4 from '../../images/logo4.svg';
 import logo5 from '../../images/logo5.svg';
+import logo6 from '../../images/logo6.svg';
 import Image from "next/image";
 export default function LogoSection(){
     return(
         <>
         
+        <div className="container">
         <div className="grid grid-cols-1">
                 <h1 className='text-5xl text-center'>FEATURED IN</h1>
-                <div className="logoDiv flex justify-center aligns-center mt-10">
-                    <Image
-                    src={logo1}
+                <div className="logoDiv flex  mt-10">
+                <Image
+                    src={logo4}
                      />
+                         <Image
+                    src={logo3}
+                     />
+                       <Image
+                    src={logo5}
+
+                     />
+                     <Image
+                     src={logo6}
+                     />
+                
                       <Image
                     src={logo2}
                      />
-                      <Image
-                    src={logo3}
+                       <Image
+                    src={logo1}
                      />
-                      <Image
-                    src={logo4}
-                     />
-                      <Image
-                    src={logo5}
-                     />
+                  
+                   
                     
                 </div>
             </div>
+        </div>
+       
         </>
     )
 }
